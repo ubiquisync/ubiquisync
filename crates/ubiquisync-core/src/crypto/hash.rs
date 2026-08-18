e4a053cb669bb9df6844a02aae5a4d3d540ff637
Aaron C <aaronc@users.noreply.github.com>
2026-08-18 15:17:06 -0400

    snapshot tests

--- crates/ubiquisync-core/src/crypto/hash.rs
+++ crates/ubiquisync-core/src/crypto/hash.rs
@@ -35,7 +35,7 @@ enum HasherInternal {
 }
 
 #[derive(IntoStaticStr, EnumIter, Debug, Clone, Copy, PartialEq, Eq)]
-#[strum(prefix = "ubiquisync/v1/")]
+#[strum(prefix = "ubiquisync/v1/hash/")]
 pub enum TaggedHashDomain {
     MmrNode,
     MmrSeed,
@@ -43,8 +43,17 @@ pub enum TaggedHashDomain {
     MmrSignBytes,
 }
 
-#[derive(Debug, Clone, Copy)]
-pub struct KdfDomain(&'static str);
+#[derive(IntoStaticStr, EnumIter, Debug, Clone, Copy, PartialEq, Eq)]
+#[strum(prefix = "ubiquisync/v1/kdf/")]
+pub enum DeriveKeyDomain {
+    EncryptionKey,
+}
+
+#[derive(IntoStaticStr, EnumIter, Debug, Clone, Copy, PartialEq, Eq)]
+#[strum(prefix = "ubiquisync/v1/fingerprint/")]
+pub enum KeyFingerprintDomain {
+    EncryptionKey,
+}
 
 #[derive(Error, Debug)]
 pub enum Hash256SuiteDecodeError {
@@ -81,10 +90,7 @@ impl Hash256Suite {
     }
 
     fn tagged_hasher_internal(&self, domain: &str) -> Hasher {
-        let len: u8 = domain
-            .len()
-            .try_into()
-            .expect("domain string should have len <= 255");
+        let len: u8 = domain_len(domain);
         Hasher(match self {
             // TODO feature flags for supported hashes at build time
             Hash256Suite::Sha256 => {
@@ -100,30 +106,30 @@ impl Hash256Suite {
         })
     }
 
-    pub fn derive_key(&self, domain: KdfDomain, key: &Key256, info: &[u8]) -> Key256 {
+    pub fn derive_key(&self, domain: DeriveKeyDomain, key: &Key256, info: &[u8]) -> Key256 {
         Key256(SecretBox::<[u8; 32]>::init_with_mut(|output| {
-            self.kdf(domain, key, info, output)
+            self.kdf(domain.into(), key, info, output)
         }))
     }
 
-    pub fn key_fingerprint(&self, domain: KdfDomain, key: &Key256) -> Key256Fingerprint {
+    pub fn key_fingerprint(&self, domain: KeyFingerprintDomain, key: &Key256) -> Key256Fingerprint {
         let mut output = [0; 32];
-        self.kdf(domain, key, &[], &mut output);
+        self.kdf(domain.into(), key, &[], &mut output);
         Key256Fingerprint(output)
     }
 
-    fn kdf(&self, domain: KdfDomain, key: &Key256, info: &[u8], output: &mut [u8; 32]) {
-        let domain_len = domain.len();
+    fn kdf(&self, domain: &str, key: &Key256, info: &[u8], output: &mut [u8; 32]) {
+        let len: u8 = domain_len(domain);
         match self {
             Hash256Suite::Sha256 => {
                 let hk = Hkdf::<Sha256>::from_prk(key.0.expose_secret()).expect("32 byte key");
-                hk.expand_multi_info(&[&[domain_len], domain.0.as_bytes(), info], output)
+                hk.expand_multi_info(&[&[len], domain.as_bytes(), info], output)
                     .expect("valid lengths");
             }
             Hash256Suite::Blake3 => {
                 let mut hasher = blake3::Hasher::new_keyed(key.0.expose_secret());
-                hasher.update(&[domain_len]);
-                hasher.update(domain.0.as_bytes());
+                hasher.update(&[len]);
+                hasher.update(domain.as_bytes());
                 hasher.update(info);
                 hasher.finalize_xof().fill(output);
             }
@@ -131,15 +137,11 @@ impl Hash256Suite {
     }
 }
 
-impl KdfDomain {
-    pub const fn new(domain: &'static str) -> Self {
-        assert!(domain.len() < 256, "KdfDomain is too long");
-        Self(domain)
-    }
-
-    fn len(&self) -> u8 {
-        self.0.len().try_into().expect("max length 255")
-    }
+fn domain_len(domain: &str) -> u8 {
+    domain
+        .len()
+        .try_into()
+        .expect("domain string should have len <= 255")
 }
 
 impl Hasher {
@@ -171,7 +173,9 @@ mod tests {
     use std::fmt::Write;
     use strum::IntoEnumIterator;
 
-    use crate::crypto::{Hash256Suite, KdfDomain, Key256, TaggedHashDomain};
+    use crate::crypto::{
+        DeriveKeyDomain, Hash256Suite, Key256, KeyFingerprintDomain, TaggedHashDomain,
+    };
 
     #[test]
     fn test_kdf_len_prefixed() {
@@ -183,8 +187,8 @@ mod tests {
         let key = Key256(SecretBox::new(Box::new([1; 32])));
         let mut x = [0; 32];
         let mut y = [0; 32];
-        suite.kdf(KdfDomain::new("a"), &key, b"bc", &mut x);
-        suite.kdf(KdfDomain::new("ab"), &key, b"c", &mut y);
+        suite.kdf("a", &key, b"bc", &mut x);
+        suite.kdf("ab", &key, b"c", &mut y);
         assert_ne!(x, [0; 32]);
         assert_ne!(y, [0; 32]);
         assert_ne!(x, y);
@@ -202,47 +206,48 @@ mod tests {
         assert_ne!(x, y)
     }
 
-    #[test]
-    fn test_known_sha256() {
-        test_known_hashes(
-            Hash256Suite::Sha256,
-            &[
-                "1e18834c426d00e57788444cb3ccd62c771b420c095bb0c4e040a8c122c4570d",
-                "3d723448c579b5e3e5d771bc0e5deda551db6cc198b0bc975bd30ac6584b62d8",
-                "158c46d846b5017928100e816969369dd5fdea2b379fda352922b97ef85045c5",
-            ],
-        );
-    }
+    // TODO is this stuff even needed anymore??
+    // #[test]
+    // fn test_known_sha256() {
+    //     test_known_hashes(
+    //         Hash256Suite::Sha256,
+    //         &[
+    //             "1e18834c426d00e57788444cb3ccd62c771b420c095bb0c4e040a8c122c4570d",
+    //             "3d723448c579b5e3e5d771bc0e5deda551db6cc198b0bc975bd30ac6584b62d8",
+    //             "158c46d846b5017928100e816969369dd5fdea2b379fda352922b97ef85045c5",
+    //         ],
+    //     );
+    // }
 
-    #[test]
-    fn test_known_blake3() {
-        test_known_hashes(
-            Hash256Suite::Blake3,
-            &[
-                "d12db47e19b120ba45062e4ff453d3d40d1d7c2b8a0ca07c9fe4945d283aee13",
-                "4bf31948ffa28caf02dc226346de69d7f2fecf36f3b86e1c3d4cdd508c21a3a7",
-                "aab5ab04ca17deb69363735d6c021919d7bca1fcccac66850728af543fa95233",
-            ],
-        );
-    }
+    // #[test]
+    // fn test_known_blake3() {
+    //     test_known_hashes(
+    //         Hash256Suite::Blake3,
+    //         &[
+    //             "d12db47e19b120ba45062e4ff453d3d40d1d7c2b8a0ca07c9fe4945d283aee13",
+    //             "4bf31948ffa28caf02dc226346de69d7f2fecf36f3b86e1c3d4cdd508c21a3a7",
+    //             "aab5ab04ca17deb69363735d6c021919d7bca1fcccac66850728af543fa95233",
+    //         ],
+    //     );
+    // }
+    //
+    // fn test_known_hashes(suite: Hash256Suite, known: &[&str; 3]) {
+    //     let a = suite.tagged_hash_internal("a", b"bc");
+    //     assert_eq!(hex(&a), known[0]);
 
-    fn test_known_hashes(suite: Hash256Suite, known: &[&str; 3]) {
-        let a = suite.tagged_hash_internal("a", b"bc");
-        assert_eq!(hex(&a), known[0]);
+    //     let mut h = suite.tagged_hasher_internal("a");
+    //     h.update(b"b");
+    //     h.update(b"c");
+    //     let b = h.finalize();
+    //     assert_eq!(hex(&b), known[0]);
 
-        let mut h = suite.tagged_hasher_internal("a");
-        h.update(b"b");
-        h.update(b"c");
-        let b = h.finalize();
-        assert_eq!(hex(&b), known[0]);
+    //     let key = Key256(SecretBox::new(Box::new([1; 32])));
+    //     let c = suite.key_fingerprint("b", &key);
+    //     assert_eq!(hex(&c.0), known[1]);
 
-        let key = Key256(SecretBox::new(Box::new([1; 32])));
-        let c = suite.key_fingerprint(KdfDomain::new("b"), &key);
-        assert_eq!(hex(&c.0), known[1]);
-
-        let d = suite.derive_key(KdfDomain::new("c"), &key, b"xyz");
-        assert_eq!(hex(&d.0.expose_secret()), known[2]);
-    }
+    //     let d = suite.derive_key("c", &key, b"xyz");
+    //     assert_eq!(hex(&d.0.expose_secret()), known[2]);
+    // }
 
     fn hex(hash: &[u8; 32]) -> String {
         let mut s = String::new();
@@ -264,12 +269,48 @@ mod tests {
 
     // prevents hash regressions from renaming items in the domain enum
     fn test_domain_regression(suite: Hash256Suite) -> String {
-        let mut out = String::from(format!("{suite:?}\n"));
+        let mut out = format!("{suite:?}\n");
+        let key = Key256(SecretBox::new(Box::new([1; 32])));
+
         for domain in TaggedHashDomain::iter() {
             let h = suite.tagged_hash(domain, b"abcdefg");
             let d: &str = domain.into();
             writeln!(&mut out, "{0} = {1}", d, hex(&h)).unwrap();
+
+            // check that both hash methods produce the same output
+            let mut hasher = suite.tagged_hasher(domain);
+            hasher.update(b"abcdefg");
+            let h2 = hasher.finalize();
+            assert_eq!(h, h2);
+        }
+
+        for domain in DeriveKeyDomain::iter() {
+            let d: &str = domain.into();
+            let k = suite.derive_key(domain, &key, b"");
+            writeln!(&mut out, "{0} = {1}", d, hex(k.0.expose_secret())).unwrap();
+
+            let k2 = suite.derive_key(domain, &key, b"xyz");
+            writeln!(&mut out, "{0} / xyz = {1}", d, hex(k2.0.expose_secret())).unwrap();
+        }
+
+        for domain in KeyFingerprintDomain::iter() {
+            let f = suite.key_fingerprint(domain, &key);
+            let d: &str = domain.into();
+            writeln!(&mut out, "{0} = {1}", d, hex(&f.0)).unwrap();
         }
         out
+    }
+
+    #[test]
+    fn test_domain_lengths() {
+        for domain in TaggedHashDomain::iter() {
+            let s: &str = domain.into();
+            assert!(s.len() <= 255);
+        }
+
+        for domain in DeriveKeyDomain::iter() {
+            let s: &str = domain.into();
+            assert!(s.len() <= 255);
+        }
     }
 }
