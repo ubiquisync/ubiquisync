53ea47f7a3800a8dd3c92e1a9831d5dc9f204965
Aaron C <aaronc@users.noreply.github.com>
2026-08-18 13:48:53 -0400

    wip on hashing, errors
    temporarily disabling some stuff for testing

--- crates/ubiquisync-core/src/crypto/hash.rs
+++ crates/ubiquisync-core/src/crypto/hash.rs
@@ -2,11 +2,14 @@ use hkdf::Hkdf;
 use num_enum::{IntoPrimitive, TryFromPrimitive};
 use secrecy::{ExposeSecret, SecretBox};
 use sha2::{Digest, Sha256};
+use thiserror::Error;
 
 use crate::{
-    codec::{reader::Reader, writer::Writer},
+    codec::{
+        reader::{ReadError, Reader},
+        writer::Writer,
+    },
     crypto::{Key256, Key256Fingerprint},
-    log_entry::DecodeError,
 };
 
 pub type Hash256 = [u8; 32];
@@ -36,14 +39,23 @@ pub struct TaggedHashDomain(&'static str);
 #[derive(Debug, Clone, Copy)]
 pub struct KdfDomain(&'static str);
 
+#[derive(Error, Debug)]
+pub enum Hash256SuiteDecodeError {
+    #[error("unknown hash suite: {0}")]
+    UnknownHashSuite(u8),
+    #[error("read error: {0}")]
+    ReadError(#[from] ReadError),
+}
+
 impl Hash256Suite {
     pub fn encode(&self, writer: &mut Writer) {
         writer.write_byte((*self).into())
     }
 
-    pub fn decode(reader: &mut Reader) -> Result<Self, DecodeError> {
+    pub fn decode(reader: &mut Reader) -> Result<Self, Hash256SuiteDecodeError> {
         let x = reader.read_byte()?;
-        let suite = Self::try_from_primitive(x).map_err(|_| DecodeError::UnknownHashSuite(x))?;
+        let suite = Self::try_from_primitive(x)
+            .map_err(|_| Hash256SuiteDecodeError::UnknownHashSuite(x))?;
         Ok(suite)
     }
 
@@ -58,11 +70,7 @@ impl Hash256Suite {
             // TODO feature flags for supported hashes at build time
             Hash256Suite::Sha256 => {
                 let mut hasher = Sha256::new();
-                hasher.update(&[domain
-                    .0
-                    .len()
-                    .try_into()
-                    .expect("length should statically be < 256")]);
+                hasher.update(&[domain.len()]);
                 hasher.update(domain.0);
                 HasherInternal::Sha256(hasher)
             }
@@ -74,26 +82,33 @@ impl Hash256Suite {
     }
 
     pub fn derive_key(&self, domain: KdfDomain, key: &Key256, info: &[u8]) -> Key256 {
-        match self {
-            Hash256Suite::Sha256 => {
-                let hk = Hkdf::<Sha256>::from_prk(key.0.expose_secret()).expect("32 byte key");
-                let mut okm = [0; 32];
-                let domain_len: u8 = domain.0.len().try_into().expect("max length 255");
-                hk.expand_multi_info(&[&[domain_len], domain.0.as_bytes(), info], &mut okm)
-                    .expect("valid lengths");
-                Key256(SecretBox::new(Box::new(okm)))
-            }
-            Hash256Suite::Blake3 => {
-                let mut hasher = blake3::Hasher::new_keyed(key.0.expose_secret());
-                hasher.update(domain.0.as_bytes());
-                hasher.update(info);
-                Key256(SecretBox::new(Box::new(hasher.finalize().into())))
-            }
-        }
+        Key256(SecretBox::<[u8; 32]>::init_with_mut(|output| {
+            self.kdf(domain, key, info, output)
+        }))
     }
 
     pub fn key_fingerprint(&self, domain: KdfDomain, key: &Key256) -> Key256Fingerprint {
-        Key256Fingerprint(*self.derive_key(domain, key, &[]).0.expose_secret())
+        let mut output = [0; 32];
+        self.kdf(domain, key, &[], &mut output);
+        Key256Fingerprint(output)
+    }
+
+    fn kdf(&self, domain: KdfDomain, key: &Key256, info: &[u8], output: &mut [u8; 32]) {
+        let domain_len = domain.len();
+        match self {
+            Hash256Suite::Sha256 => {
+                let hk = Hkdf::<Sha256>::from_prk(key.0.expose_secret()).expect("32 byte key");
+                hk.expand_multi_info(&[&[domain_len], domain.0.as_bytes(), info], output)
+                    .expect("valid lengths");
+            }
+            Hash256Suite::Blake3 => {
+                let mut hasher = blake3::Hasher::new_keyed(key.0.expose_secret());
+                hasher.update(&[domain_len]);
+                hasher.update(domain.0.as_bytes());
+                hasher.update(info);
+                hasher.finalize_xof().fill(output);
+            }
+        }
     }
 }
 
@@ -102,12 +117,20 @@ impl TaggedHashDomain {
         assert!(domain.len() < 256, "TaggedHashDomain is too long");
         Self(domain)
     }
+
+    pub fn len(&self) -> u8 {
+        self.0.len().try_into().expect("max length 255")
+    }
 }
 
 impl KdfDomain {
     pub const fn new(domain: &'static str) -> Self {
         assert!(domain.len() < 256, "KdfDomain is too long");
         Self(domain)
+    }
+
+    pub fn len(&self) -> u8 {
+        self.0.len().try_into().expect("max length 255")
     }
 }
 
@@ -130,5 +153,41 @@ impl Hasher {
             HasherInternal::Sha256(hasher) => hasher.finalize().into(),
             HasherInternal::Blake3(hasher) => hasher.finalize().into(),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use secrecy::SecretBox;
+
+    use crate::crypto::{Hash256Suite, KdfDomain, Key256, TaggedHashDomain};
+
+    #[test]
+    fn test_kdf_len_prefixed() {
+        do_test_kdf_len_prefixed(Hash256Suite::Sha256);
+        do_test_kdf_len_prefixed(Hash256Suite::Blake3);
+    }
+
+    fn do_test_kdf_len_prefixed(suite: Hash256Suite) {
+        let key = Key256(SecretBox::new(Box::new([1; 32])));
+        let mut x = [0; 32];
+        let mut y = [0; 32];
+        suite.kdf(KdfDomain::new(&"a"), &key, b"bc", &mut x);
+        suite.kdf(KdfDomain::new(&"ab"), &key, b"c", &mut y);
+        assert_ne!(x, [0; 32]);
+        assert_ne!(y, [0; 32]);
+        assert_ne!(x, y);
+    }
+
+    #[test]
+    fn test_tagged_hash_len_prefixed() {
+        do_test_tagged_hash_len_prefixed(Hash256Suite::Sha256);
+        do_test_tagged_hash_len_prefixed(Hash256Suite::Blake3);
+    }
+
+    fn do_test_tagged_hash_len_prefixed(suite: Hash256Suite) {
+        let x = suite.tagged_hash(TaggedHashDomain::new(&"a"), b"bc");
+        let y = suite.tagged_hash(TaggedHashDomain::new(&"ab"), b"c");
+        assert_ne!(x, y)
     }
 }
--- crates/ubiquisync-core/src/log_entry/error.rs
+++ crates/ubiquisync-core/src/log_entry/error.rs
@@ -1,6 +1,10 @@
 use thiserror::Error;
 
-use crate::{codec::reader::ReadError, crypto::Key256Fingerprint, hlc::Timestamp, init::Version};
+use crate::{
+    codec::reader::ReadError,
+    crypto::{Key256Fingerprint, SignatureVerificationError},
+    hlc::Timestamp,
+};
 
 #[derive(Error, Debug)]
 pub enum EncodeError {
@@ -10,29 +14,29 @@ pub enum EncodeError {
     InvalidExpungeRange,
 }
 
-#[derive(Error, Debug)]
-pub enum DecodeError {
-    #[error("read error: {0}")]
-    ReadError(#[from] ReadError),
-    #[error("invalid server attested user id length: {length}")]
-    InvalidServerAttestedId { length: usize },
-    #[error("unknown cipher suite: {0}")]
-    UnknownCipherSuite(u8),
-    #[error("unknown signature algorithm: {0}")]
-    UnknownSignatureAlgorithm(u8),
-    #[error("unknown key-exchange algorithm: {0}")]
-    UnknownKeyExchangeAlgorithm(u8),
-    #[error("unknown hash suite: {0}")]
-    UnknownHashSuite(u8),
-    #[error("unexpected entry type: {0}")]
-    UnexpectedEntryType(u8),
-    #[error("unsupported version: {0}")]
-    UnsupportedVersion(u8),
-    #[error("unknown init flags: {0}")]
-    UnknownInitFlags(u8),
-    #[error("unknown init data, {remaining} unreadable bytes, version: {version:?}")]
-    UnknownInitData { version: Version, remaining: usize },
-}
+// #[derive(Error, Debug)]
+// pub enum DecodeError {
+//     #[error("read error: {0}")]
+//     ReadError(#[from] ReadError),
+//     #[error("invalid server attested user id length: {length}")]
+//     InvalidServerAttestedId { length: usize },
+//     #[error("unknown cipher suite: {0}")]
+//     UnknownCipherSuite(u8),
+//     #[error("unknown signature algorithm: {0}")]
+//     UnknownSignatureAlgorithm(u8),
+//     #[error("unknown key-exchange algorithm: {0}")]
+//     UnknownKeyExchangeAlgorithm(u8),
+//     #[error("unknown hash suite: {0}")]
+//     UnknownHashSuite(u8),
+//     #[error("unexpected entry type: {0}")]
+//     UnexpectedEntryType(u8),
+//     #[error("unsupported version: {0}")]
+//     UnsupportedVersion(u8),
+//     #[error("unknown init flags: {0}")]
+//     UnknownInitFlags(u8),
+//     #[error("unknown init data, {remaining} unreadable bytes, version: {version:?}")]
+//     UnknownInitData { version: Version, remaining: usize },
+// }
 
 #[derive(Error, Debug)]
 pub enum DataIngestError {
@@ -40,14 +44,8 @@ pub enum DataIngestError {
     DataCorruption(#[from] ReadError),
     #[error("incompatible software version: {0}")]
     Version(#[from] SoftwareVersionError),
-}
-
-#[derive(Error, Debug)]
-pub enum LogProcessError {
-    #[error("forward clock skew: {0:? }")]
-    ForwardClockSkew(Timestamp /* derive Display to make human readable */),
-    #[error("missing decryption key: {0:?}")]
-    MissingDecryptionKey(Key256Fingerprint),
+    #[error("signature verification failed: {0}")]
+    SignatureVerificationFailed(SignatureVerificationError),
 }
 
 #[derive(Error, Debug)]
@@ -60,16 +58,32 @@ pub enum DataCorruptionError {
 
 #[derive(Error, Debug)]
 pub enum SoftwareVersionError {
-    #[error("unknown cipher suite: {0}")]
-    UnknownCipherSuite(u8),
     #[error("unknown signature algorithm: {0}")]
     UnknownSignatureAlgorithm(u8),
-    #[error("unknown key-exchange algorithm: {0}")]
-    UnknownKeyExchangeAlgorithm(u8),
     #[error("unknown hash suite: {0}")]
     UnknownHashSuite(u8),
     #[error("unexpected entry type: {0}")]
     UnexpectedEntryType(u8),
     #[error("unsupported version: {0}")]
     UnsupportedVersion(u8),
+}
+
+#[derive(Error, Debug)]
+pub enum LogProcessError {
+    #[error("unknown cipher suite: {0}")]
+    UnknownCipherSuite(u8),
+    #[error("missing decryption key: {0:?}")]
+    MissingDecryptionKey(Key256Fingerprint),
+    #[error("clock reversal: {last:?} -> {next:?}")]
+    ClockReversal { last: Timestamp, next: Timestamp },
+    #[error("forward clock skew: {0:? }")]
+    ForwardClockSkew(Timestamp /* derive Display to make human readable */),
+}
+
+pub enum CommitError {
+    UnknownOpType(u8),
+    CorruptOp,
+    OutOfSpace,
+    BusyUnreachable,
+    NeedReplay,
 }
