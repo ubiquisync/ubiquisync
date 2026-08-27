8bdd05905b55e3fe29551932b801a61631ad1ce2
Aaron C <aaronc@users.noreply.github.com>
2026-08-27 14:35:27 -0400

    update replica sql design

--- docs/design2.sql
+++ docs/design2.sql
@@ -1,18 +1,41 @@
 CREATE TABLE peers (
-    id INT AUTOINCREMENT,
+    id INT AUTOINCREMENT PRIMARY KEY,
     peer_id BYTES NOT NULL UNIQUE,
     commitment_bytes BYTES NOT NULL,
     signature BYTES NOT NULL
-) WITHOUT ROWID;
+);
+
+CREATE TABLE containers (
+    id INT AUTOINCREMENT PRIMARY KEY,
+    container_id UUID NOT NULL UNIQUE
+);
+
+CREATE TABLE logs (
+    id INT AUTOINCREMENT PRIMARY KEY,
+    peer_id INT NOT NULL REFERENCES peers(id),
+    container_id INT NOT NULL REFERENCES containers(id),
+    parent_id INT NULL REFERENCES logs(id),
+    fork_idx INT NULL,
+    fork_hash BYTES NULL,
+    ready_idx INT NULL,
+    ready_status INT NULL,
+    ready_status_data BYTES NULL,
+    commit_idx INT NULL,
+    commit_status INT NULL,
+    commit_status_data BYTES NULL,
+    CHECK((fork_idx IS NULL) = (fork_hash IS NULL)),
+    CHECK((parent_id IS NOT NULL) OR (fork_idx IS NULL))
+);
+
+CREATE UNIQUE INDEX logs_root ON logs(peer_id, container_id) WHERE parent_id IS NULL;
 
 CREATE TABLE segments (
-    stream_id BYTES NOT NULL,
+    id INT AUTOINCREMENT PRIMARY KEY,
+    log_id INT NOT NULL REFERENCES logs(id),
     start_idx INT NOT NULL,
-    peer_id INT NOT NULL,
-    container_id UUID NOT NULL,
-    end_idx INT NOT NULL
+    end_idx INT NOT NULL,
     end_hash BYTES NOT NULL,
     body BYTES NOT NULL,
-    status BYTES NULL
-    PRIMARY KEY(stream_id, start_idx)
 );
+
+CREATE INDEX ON segment_log_end segments(log_id, end_idx);
