80e7c5a2e91b347a0514794c71d6a8dff30722da
Aaron C <aaronc@users.noreply.github.com>
2026-09-10 00:06:17 -0400

    pack file design

--- crates/ubiquisync-fs/src/pack/header.rs
+++ crates/ubiquisync-fs/src/pack/header.rs
@@ -2,15 +2,26 @@ use std::ops::Range;
 
 use ubiquisync_core::{
     codec::{ReadError, Reader, WriteError, Writer},
+    crypto::Hash256,
     ids::{ContainerId, PeerId},
-    log::ChainHash,
 };
 
 #[derive(Debug, Clone)]
 pub struct PackHeader {
     pub parents: Vec<PackRef>,
+    /// The list of packs this pack file supersedes directly.
+    /// This is used by GC to know when it is safe to delete a pack
+    /// which has been compressed.
+    /// It is NOT necessary to reference packs which were superseded
+    /// by prior compression rounds and doing so just unnecessarily
+    /// bloats the header file.
+    /// The safe GC condition is simply: is there a pack file which
+    /// names the pack ref in its file name (with a later generation)
+    /// or in its supersedes list directly, and has a sufficient amount
+    /// of time elapsed since that file was written (as a safety buffer).
     pub supersedes: Vec<PackRef>,
     pub self_segments: Vec<SegmentDescriptor>,
+    pub peer_data: Vec<PeerData>,
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
@@ -20,16 +31,18 @@ pub struct PackRef {
 }
 
 #[derive(Debug, Clone)]
-pub struct ForeignSegments {
+pub struct PeerData {
     pub peer_id: PeerId,
+    pub supersedes: Vec<PackRef>,
     pub segments: Vec<SegmentDescriptor>,
 }
 
 #[derive(Debug, Clone)]
 pub struct SegmentDescriptor {
     pub container_id: ContainerId,
-    pub prev_chain: ChainHash,
-    pub end_chain: ChainHash,
+    pub idx_range: Range<u64>,
+    pub prev_chain: Hash256,
+    pub end_chain: Hash256,
     pub body_loc: Range<u64>,
 }
 
@@ -43,61 +56,61 @@ impl PackHeader {
     }
 }
 
-impl PackRef {
-    pub fn encode(&self, w: &mut Writer) {
-        // fixed length encoding because id is a random number
-        w.write_le_u64(self.id);
-        w.write_var_u64(self.end_seq);
-    }
+// impl PackRef {
+//     pub fn encode(&self, w: &mut Writer) {
+//         // fixed length encoding because id is a random number
+//         w.write_le_u64(self.id);
+//         w.write_var_u64(self.end_seq);
+//     }
 
-    pub fn decode(r: &mut Reader) -> Result<Self, ReadError> {
-        let id = r.read_le_u64()?;
-        let end_seq = r.read_var_u64()?;
-        Ok(Self { id, end_seq })
-    }
-}
+//     pub fn decode(r: &mut Reader) -> Result<Self, ReadError> {
+//         let id = r.read_le_u64()?;
+//         let end_seq = r.read_var_u64()?;
+//         Ok(Self { id, end_seq })
+//     }
+// }
 
-impl ForeignSegments {
-    pub fn encode(&self, w: &mut Writer) -> Result<(), WriteError> {
-        w.write_array(&self.peer_id.0);
-        w.write_var_usize(self.segments.len());
-        for s in self.segments.iter() {
-            s.encode(w)?;
-        }
-        Ok(())
-    }
+// impl ForeignSegments {
+//     pub fn encode(&self, w: &mut Writer) -> Result<(), WriteError> {
+//         w.write_array(&self.peer_id.0);
+//         w.write_var_usize(self.segments.len());
+//         for s in self.segments.iter() {
+//             s.encode(w)?;
+//         }
+//         Ok(())
+//     }
 
-    pub fn decode(r: &mut Reader) -> Result<Self, ReadError> {
-        let peer_id = PeerId(r.read_array()?);
-        let n = r.read_var_usize()?;
-        // NOTE: do not reserve an array to avoid OOM attacks!
-        let mut segments = vec![];
-        for _ in 0..n {
-            segments.push(SegmentDescriptor::decode(r)?);
-        }
-        Ok(Self { peer_id, segments })
-    }
-}
+//     pub fn decode(r: &mut Reader) -> Result<Self, ReadError> {
+//         let peer_id = PeerId(r.read_array()?);
+//         let n = r.read_var_usize()?;
+//         // NOTE: do not reserve an array to avoid OOM attacks!
+//         let mut segments = vec![];
+//         for _ in 0..n {
+//             segments.push(SegmentDescriptor::decode(r)?);
+//         }
+//         Ok(Self { peer_id, segments })
+//     }
+// }
 
-impl SegmentDescriptor {
-    pub fn encode(&self, w: &mut Writer) -> Result<(), WriteError> {
-        w.write_array(&self.container_id.0);
-        self.prev_chain.encode(w);
-        self.end_chain.encode(w);
-        w.write_range(&self.body_loc)?;
-        Ok(())
-    }
+// impl SegmentDescriptor {
+//     pub fn encode(&self, w: &mut Writer) -> Result<(), WriteError> {
+//         w.write_array(&self.container_id.0);
+//         self.prev_chain.encode(w);
+//         self.end_chain.encode(w);
+//         w.write_range(&self.body_loc)?;
+//         Ok(())
+//     }
 
-    pub fn decode(r: &mut Reader) -> Result<Self, ReadError> {
-        let container_id = ContainerId(r.read_array()?);
-        let prev_chain = ChainHash::decode(r)?;
-        let end_chain = ChainHash::decode(r)?;
-        let body_loc = r.read_range()?;
-        Ok(Self {
-            container_id,
-            prev_chain,
-            end_chain,
-            body_loc,
-        })
-    }
-}
+//     pub fn decode(r: &mut Reader) -> Result<Self, ReadError> {
+//         let container_id = ContainerId(r.read_array()?);
+//         let prev_chain = ChainHash::decode(r)?;
+//         let end_chain = ChainHash::decode(r)?;
+//         let body_loc = r.read_range()?;
+//         Ok(Self {
+//             container_id,
+//             prev_chain,
+//             end_chain,
+//             body_loc,
+//         })
+//     }
+// }
--- crates/ubiquisync-fs/src/pack/snapshot.rs
+++ crates/ubiquisync-fs/src/pack/snapshot.rs
@@ -1,37 +1,84 @@
-use std::collections::{HashMap, HashSet, hash_map::Entry};
+use std::{
+    collections::{
+        HashMap, HashSet,
+        hash_map::{self, Entry},
+    },
+    ops::Range,
+};
 
 use crate::pack::{PackFileName, PackRef};
 
+#[derive(Debug, Clone)]
 pub struct PackReadState {
     /// The packs we have already inspected either
     /// directly or in a prior generation.
     /// These get saved in state.
-    pub consumed: Vec<PackRef>,
+    pub consumed: HashSet<PackRef>,
+
+    pub blocked: Vec<BlockedPackRef>,
+}
+
+#[derive(Debug, Clone)]
+pub struct PackReadResult {
+    pub new_state: PackReadState,
+
     /// The files we need to inspect. We only retain this
     /// transiently between directory listings and add files
     /// to the consumed state as we inspect them.
     pub to_read: Vec<PackFileName>,
 }
 
-/// `consumed` and `cur` must be deduped already
-pub fn compute_read_state(consumed: &[PackRef], cur: &[PackFileName]) -> PackReadState {
-    let consumed_set = consumed.iter().collect::<HashSet<_>>();
-    let mut to_read = vec![];
-    let mut new_consumed = vec![];
-    for f in cur.iter() {
-        let r = f.get_ref();
-        if consumed_set.contains(&r) {
-            new_consumed.push(r);
-        } else {
-            to_read.push(f.clone());
+#[derive(Debug, Clone)]
+pub struct BlockedPackRef {
+    pub file: PackFileName,
+    pub parents: HashSet<PackRef>,
+    pub read_timestamps: Range<u64>,
+    pub retries: u64,
+}
+
+impl PackReadState {
+    pub fn update(&self, ts: u64, cur_files: &[PackFileName]) -> PackReadResult {
+        let mut to_read = vec![];
+        let mut new_consumed = HashSet::new();
+        for f in cur_files.iter() {
+            let r = f.get_ref();
+            if self.consumed.contains(&r) {
+                new_consumed.insert(r);
+            } else {
+                to_read.push(f.clone());
+            }
+        }
+        to_read.sort_by_key(|v| v.seqs.end);
+        // TODO handled blocked packs
+        PackReadResult {
+            new_state: PackReadState {
+                consumed: new_consumed,
+                blocked: self.blocked.clone(),
+            },
+            to_read,
         }
     }
-    to_read.sort_by_key(|v| v.seqs.end);
-    PackReadState {
-        consumed: new_consumed,
-        to_read,
-    }
 }
+
+// /// `consumed` and `cur` must be deduped already
+// pub fn compute_read_state(consumed: &[PackRef], cur: &[PackFileName]) -> PackReadState {
+//     let consumed_set = consumed.iter().collect::<HashSet<_>>();
+//     let mut to_read = vec![];
+//     let mut new_consumed = vec![];
+//     for f in cur.iter() {
+//         let r = f.get_ref();
+//         if consumed_set.contains(&r) {
+//             new_consumed.push(r);
+//         } else {
+//             to_read.push(f.clone());
+//         }
+//     }
+//     to_read.sort_by_key(|v| v.seqs.end);
+//     PackReadState {
+//         consumed: new_consumed,
+//         to_read,
+//     }
+// }
 
 /// Dedupes files and chooses the latest valid generation of a pack.
 /// NOTE: if a generation file is corrupted garbage we could inspect
